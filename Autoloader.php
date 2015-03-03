13a7bcf3e56d3033d655653cf553036640c3614c
Ivan Enderlin <ivan.enderlin@hoa-project.net>
2015-03-03 09:17:34 +0100

    Fix copyright and CS.

--- Autoloader.php
+++ Autoloader.php
@@ -8,7 +8,7 @@
  *
  * New BSD License
  *
- * Copyright © 2007-2014, Ivan Enderlin. All rights reserved.
+ * Copyright © 2007-2015, Ivan Enderlin. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
@@ -40,10 +40,10 @@ use mageekguy\atoum;
 
 $vendorDirectory = __DIR__ . '/vendor';
 
-if (is_dir($vendorDirectory) === false)
+if(is_dir($vendorDirectory) === false)
     $vendorDirectory = __DIR__ . '/../..';
 
-if (is_file($vendorAutoloader = $vendorDirectory . '/hoa/core/Core.php'))
+if(is_file($vendorAutoloader = $vendorDirectory . '/hoa/core/Core.php'))
     require_once $vendorDirectory . '/hoa/core/Core.php';
 
 atoum\autoloader::get()
@@ -65,5 +65,4 @@ atoum\autoloader::get()
     ->addDirectory('Hoa\Router', $vendorDirectory . '/hoa/router')
     ->addDirectory('Hoa\Stream', $vendorDirectory . '/hoa/stream')
     ->addDirectory('Hoa\String', $vendorDirectory . '/hoa/string')
-    ->addDirectory('Hoa\Visitor', $vendorDirectory . '/hoa/visitor')
-;
+    ->addDirectory('Hoa\Visitor', $vendorDirectory . '/hoa/visitor');
